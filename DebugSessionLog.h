cf920a1bdb3aa87f46ffc4e3976ede05fef51adc
Maksym Lazirko <rockmastermax@gmail.com>
2026-04-15 23:04:41 -0400

    Add files via upload

---
+++ DebugSessionLog.h
@@ -0,0 +1,4 @@
+#pragma once
+
+/** NDJSON line to workspace debug-98f05a.log (session 98f05a). Thread-safe. */
+void areDbgLog (const char* hypothesisId, const char* location, const char* message, int dataA = 0, int dataB = 0);
